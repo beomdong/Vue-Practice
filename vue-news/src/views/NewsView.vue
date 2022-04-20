ff337278e8e86baf102edf3114f37155125edbce
beomdong <ehdclrltk12@naver.com>
2022-04-20 23:20:52 +0900

    styling

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div v-for="user in this.$store.state.news">{{ user.title }}</div>
+    <p v-for="item in this.$store.state.news">
+      <a v-bind:href="item.url">{{ item.title }}</a>
+      <small> {{ item.time_ago }} by {{ item.user }}</small>
+    </p>
   </div>
 </template>
 
@@ -8,15 +11,6 @@
 export default {
   created() {
     this.$store.dispatch("FETCH_NEWS");
-    // var vm = this;
-    // fetchNewsList()
-    //   .then(function (response) {
-    //     console.log(response);
-    //     vm.users = response.data;
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //   });
   },
 };
 </script>
