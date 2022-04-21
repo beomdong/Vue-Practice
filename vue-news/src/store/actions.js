246a5a5a8fa6ca2ba97c582b8a490ded3e3c68fd
beomdong <ehdclrltk12@naver.com>
2022-04-21 22:53:57 +0900

    FEAT: User 상세 페이지 구현
    api폴더  index.js 파일에 함수 추가후 export
    store폴더 actions.js파일에 api의 함수 import
    actions.js에 요청  함수작성
    views 파일의 UserView.vue 파일에 dispatch로 actions.js 함수불러오도록
    작성
    store폴더의 index.js에 user state 추가
    store폴더의 mutations.js에 요청데이터 받아서 state에 넘겨주는 함수 작성
    UserView.vue파일 탬플릿 수정

--- vue-news/src/store/actions.js
+++ vue-news/src/store/actions.js
@@ -1,35 +1,49 @@
-import { fetchNewsList, fetchJobsList, fetchAskList } from "../api/index.js";
+import {
+    fetchNewsList,
+    fetchJobsList,
+    fetchAskList,
+    fetchUserInfo,
+} from "../api/index.js";
 
 export default {
     FETCH_NEWS(context) {
         fetchNewsList()
             .then(response => {
-                console.log(response.data);
+                console.log(response.data)
                 context.commit('SET_NEWS', response.data)
                 // this.users = response.data
             })
             .catch(error => {
-                console.log(error);
+                console.log(error)
             })
     },
     FETCH_ASK({ commit }) {
         fetchAskList()
             .then(({ data }) => {
-                console.log(data);
+                console.log(data)
                 commit('SET_ASK', data)
             })
             .catch(error => {
-                console.log(error);
+                console.log(error)
             })
     },
     FETCH_JOBS({ commit }) {
         fetchJobsList()
             .then(({ data }) => {
-                console.log(data);
+                console.log(data)
                 commit('SET_JOBS', data)
             })
             .catch(error => {
-                console.log(error);
+                console.log(error)
+            })
+    },
+    FETCH_USER({ commit }, name) {
+        fetchUserInfo(name)
+            .then(({ data }) => {
+                commit('SET_USER', data)
+            })
+            .catch(error => {
+                console.log(error)
             })
     }
 }
