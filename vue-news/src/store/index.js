bca5849cf383203d421256dc8314318004bb392f
beomdong <ehdclrltk12@naver.com>
2022-04-22 02:27:47 +0900

    Style: 질문 상세 페이지 스타일링
    views폴더의 ItemView.vue 파일에 template 수정 및 style 추가
    store폴더의 index.js 파일에getters 추가
    ItemView.vue에 mapGetters 헬퍼함수 import
    ItemView.vue에 computed 속성 추가해서 mapGetters 사용
    public폴더의 index.js 파일에 fontawesone cdn 링크 추가

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -16,7 +16,10 @@ export const store = new Vuex.Store({
     getters: {
         fetchedAsk(state) {
             return state.ask
-        }
+        },
+        fetchedItem(state) {
+            return state.item
+        },
     },
     mutations,
     actions
