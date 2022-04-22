3df9965a2d95bda6a31d354a1b2bc1be6e962ba2
beomdong <ehdclrltk12@naver.com>
2022-04-22 18:53:48 +0900

    Style: NewsView.vue 파일, App.vue 파일 스타일 변경
    NewsView.vue html 전체적으로 수정
    NewsView.vue 구분선 스타일 추가
    NewsView.vue points 나타내게 하고 스타일 추가
    NewsView.vue small 태그, router-link태그에 link-text스타일 적용
    App.vue 파일 a태그 관련 스타일들 추가

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    <p v-for="item in this.$store.state.news">
-      <a v-bind:href="item.url">{{ item.title }}</a>
-      <small>
-        {{ item.time_ago }} by
-        <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
-      </small>
-    </p>
+    <ul class="new-list">
+      <li v-for="item in this.$store.state.news" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <p class="news-title">
+          <a v-bind:href="item.url">{{ item.title }}</a>
+        </p>
+        <small class="link-text">
+          by
+          <router-link :to="`/user/${item.user}`" class="link-text">{{
+            item.user
+          }}</router-link>
+        </small>
+        <!-- <a v-bind:href="item.url">{{ item.title }}</a>
+        <small>
+          {{ item.time_ago }} by
+          <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
+        </small> -->
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -18,4 +34,29 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.news-title {
+  margin: 0;
+}
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.link-text {
+  color: #828282;
+}
+</style>
