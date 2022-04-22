2069d23110eb0e53e835248d154e871508d85b3f
beomdong <ehdclrltk12@naver.com>
2022-04-22 23:42:40 +0900

    Refactor: 공동 컴포넌트 구현
    ListItem 공동 컴포넌트 구현
    news, ask, jobs 페이지 공동 컴포넌트 사용

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,65 +1,15 @@
 <template>
   <div>
-    <ul class="new-list">
-      <li v-for="item in fetchedAsk" class="post">
-        <!-- 포인트 영역 -->
-        <div class="points">
-          {{ item.points }}
-        </div>
-        <!-- 기타 정보 영역 -->
-        <div>
-          <p class="news-title">
-            <router-link :to="`item/${item.id}`">
-              {{ item.title }}
-            </router-link>
-          </p>
-          <small class="link-text">
-            {{ item.time_ago }} by
-            <router-link :to="`/user/${item.user}`" class="link-text">
-              {{ item.user }}
-            </router-link>
-          </small>
-        </div>
-      </li>
-    </ul>
+    <list-item></list-item>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import ListItem from "../components/ListItem.vue";
+
 export default {
-  computed: {
-    ...mapGetters(["fetchedAsk"]),
-  },
-  created() {
-    this.$store.dispatch("FETCH_ASK");
+  components: {
+    ListItem,
   },
 };
 </script>
-
-<style scoped>
-.news-title {
-  margin: 0;
-}
-.news-list {
-  margin: 0;
-  padding: 0;
-}
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-.points {
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #42b883;
-}
-.link-text {
-  color: #828282;
-}
-</style>
