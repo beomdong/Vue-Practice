451c4ac9e6f7843012da3a401ff3ae39b85d610b
beomdong <ehdclrltk12@naver.com>
2022-04-25 23:42:38 +0900

    REFACTOR: 비동기 처리방식 변경
    async await 방식으로 비동기 처리방식 변경

--- vue-news/src/api/index.js
+++ vue-news/src/api/index.js
@@ -17,15 +17,28 @@ const config = {
 //     return axios.get(`${config.baseUrl}jobs/1.json`)
 // }
 
-function fetchList(pageName) {
-    return axios.get(`${config.baseUrl}${pageName}/1.json`)
+async function fetchList(pageName) {
+    try {
+        return await axios.get(`${config.baseUrl}${pageName}/1.json`)
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
-function fetchUserInfo(username) {
-    return axios.get(`${config.baseUrl}user/${username}.json`)
+async function fetchUserInfo(username) {
+    try {
+        return await axios.get(`${config.baseUrl}user/${username}.json`)
+    } catch (error) {
+        console.log(error);
+    }
 }
-function fetchCommentItem(id) {
-    return axios.get(`${config.baseUrl}item/${id}.json`)
+async function fetchCommentItem(id) {
+    try {
+        return await axios.get(`${config.baseUrl}item/${id}.json`)
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
--- vue-news/src/store/actions.js
+++ vue-news/src/store/actions.js
@@ -38,28 +38,19 @@ export default {
     //             console.log(error)
     //         })
     // },
-    FETCH_USER({ commit }, name) {
-        return fetchUserInfo(name)
-            .then(({ data }) => {
-                commit('SET_USER', data)
-            })
-            .catch(error => {
-                console.log(error)
-            })
+    async FETCH_USER({ commit }, name) {
+        const response = await fetchUserInfo(name)
+        commit('SET_USER', response.data)
+        return response
     },
-    FETCH_ITEM({ commit }, id) {
-        return fetchCommentItem(id)
-            .then(({ data }) => {
-                commit('SET_ITEM', data)
-            })
-            .catch(error => {
-                console.log(error);
-            })
+    async FETCH_ITEM({ commit }, id) {
+        const response = await fetchCommentItem(id)
+        commit('SET_ITEM', response.data)
+        return response
     },
-    FETCH_LIST({ commit }, pageName) {
-        return fetchList(pageName)
-            .then(({ data }) => commit('SET_LIST', data))
-            .catch(error => console.log(error))
-
+    async FETCH_LIST({ commit }, pageName) {
+        const response = await fetchList(pageName)
+        commit('SET_LIST', response.data)
+        return response
     }
 }
