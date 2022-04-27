18baf4a7646cc6a6226df576129acb77eb476719
beomdong <ehdclrltk12@naver.com>
2022-04-28 01:33:53 +0900

    FEAT: 컴포넌트 디자인 실습 추가
    다양한 컴포넌트 디자인방법에 대해 실습하면서 파일을 만들었다.

---
+++ vue-renderless/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
+      <!-- ... response, loading 을 가져올수 있다-->
+      <template v-slot="{response, loading}">
+        <div>
+          <div v-if="!loading">
+            <p>{{response.name}}</p>
+            <p>{{response.email}}</p>
+          </div>
+          <div v-if="loading">
+            loading...
+          </div>
+        </div>
+      </template>
+    </fetch-data>
+  </div>
+</template>
+
+<script>
+import FetchData from './components/FetchData.vue'
+
+export default {
+  components: {
+    FetchData
+  },
+}
+</script>
